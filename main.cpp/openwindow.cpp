847f2057d7acb8cffd833a3d400ea25e9c88940d
Guilherme1619 <gf.ribeiro16@gmail.com>
2025-12-31 14:38:09 +0000

    updating makefile

--- main.cpp/openwindow.cpp
+++ main.cpp/openwindow.cpp
@@ -7,35 +7,10 @@ int main()
     InitWindow(width, height, "Guizado window");
 
     SetTargetFPS(60);
-    while (WindowShouldClose()==false)
+    while (WindowShouldClose()==false);
     {
         BeginDrawing();
-        ClearBackground(DarkBlue);
-
-        //Game Logic Begins
-        DrawCircle(circle_x,circle_y,50,RED);
-
-        if (IsKeyDown(KEY_D))
-        {
-            circle_x = circle_x + 10;
-        }
-
-        if (IsKeyDown(KEY_A))
-        {
-            circle_x = circle_x - 10;
-        }
-
-        if (IsKeyDown(KEY_S))
-        {
-            circle_y = circle_y + 10;
-        }
-
-        if (IsKeyDown(KEY_W))
-        {
-            circle_y = circle_y - 10;
-        }
-
-        //Game Logic Ends
+        ClearBackground(DARKBLUE);
         EndDrawing();
     }
     CloseWindow();
