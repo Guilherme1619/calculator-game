ee019a8a7a4a4588b0542b288ed4fa0127c15a6f
Guilherme1619 <gf.ribeiro16@gmail.com>
2025-12-31 18:03:06 +0000

    updating window

--- main.cpp/openwindow.cpp
+++ main.cpp/openwindow.cpp
@@ -1,18 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
 #include <raylib.h>
+using namespace std;
+
 int main()
 {
-    //window dimensions
-    int width{1050};
-    int height{600};
-    InitWindow(width, height, "Guizado window");
+    int width{1200};
+    int height{700};
+    InitWindow(width, height, "Calculator Game");
+
+    int rectangle_x{200};
+    int rectangle_y{200};
 
     SetTargetFPS(60);
     while (WindowShouldClose()==false)
     {
         BeginDrawing();
         ClearBackground(DARKBLUE);
+        DrawRectangle(rectangle_x,rectangle_y,800,300,LIGHTGRAY);
         EndDrawing();
     }
     CloseWindow();
-    return 0;
 }
